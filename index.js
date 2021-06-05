fd9c9c345f0ece08de40706de4106f860b4a948c
Akezhan Issadilov <72870074+bliny-so-smetanoi@users.noreply.github.com>
2021-06-05 23:57:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use('/api/event', require('./routes/event-routes'))
 app.use('/api/userdata', require('./routes/user-routes'))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static(path.join(__dirname, 'frontend', 'public')))
+    app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
     app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'))
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
